f0bc3f9c969b8f609daf7d98b70f8c1d71ab9296
Tim Goll <tim@timgoll.de>
2019-09-26 20:37:25 +0200

    fixing clockwork

--- lua/classes/classes/class_clockwork.lua
+++ lua/classes/classes/class_clockwork.lua
@@ -6,14 +6,14 @@ CLASS.AddClass("CLOCKWORK", {
 		color = Color(149, 188, 195, 255),
 		onActivate = function(ply)
 			if SERVER then
-				ply:EnableSlowMotion()
+				ply:EnableSlowMotion2()
 			end
 		end,
 		time = duration * timescale,
 		cooldown = cooldown,
 		unstoppable = true,
 		items = {
-			"item_ttt_slowmotion"
+			"item_ttt_sm"
 		},
 		passiveItems = {
 			"item_ttt_shootingspeed"
