43eabd60240e064058978230dae2848432f1f829
NickCloud <nicolasbeiglboeck@gmx.at>
2020-06-29 13:16:34 +0200

    Server cooldown changes (#4)
    Fixed classes that would use the clientside cooldown

--- lua/classes/classes/class_conceal.lua
+++ lua/classes/classes/class_conceal.lua
@@ -1,4 +1,6 @@
 local function ConcealFunction(ply)
+	if CLIENT then return end
+
 	-- Traces a line from the players shoot position to 100 units
 	local pos = ply:GetShootPos()
 	local ang = ply:GetAimVector()
