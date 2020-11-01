07db2d3f4758c63b60999ca9343e527a4cf03b04
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-01 22:56:46 +0100

    Update class_clockwork.lua

--- lua/classes/classes/class_clockwork.lua
+++ lua/classes/classes/class_clockwork.lua
@@ -22,10 +22,12 @@ CLASS.AddClass("CLOCKWORK", {
 	lang = {
 		name = {
 			English = "Clockwork",
+			French = "Horloger",	
 			Русский = "Часовой"
 		},
 		desc = {
 			English = "The weapons of the clockwork have a faster shooting speed than they would normally have. Additionally, they are able to slow down the time for 3 seconds once every 45 seconds.",
+			French = "Les armes de l'Horloger ont une cadence de tir plus rapide. De plus, elles sont capables de ralentir le temps de 3 secondes une fois toutes les 45 secondes.",	
 			Русский = "Оружие часового имеет более высокую скорость стрельбы, чем обычно. Кроме того, он может замедлять время на 3 секунды каждые 45 секунд."
 		}
 	}
