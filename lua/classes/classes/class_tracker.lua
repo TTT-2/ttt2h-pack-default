216eae89af195fc55d0c8e95b0c009bdd9c6c697
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-01 21:10:31 +0100

    Update class_tracker.lua

--- lua/classes/classes/class_tracker.lua
+++ lua/classes/classes/class_tracker.lua
@@ -9,10 +9,12 @@ CLASS.AddClass("TRACKER", {
 	lang = {
 		name = {
 			English = "Tracker",
+			French = "Traqueur",	
 			Русский = "Трекер"
 		},
 		desc = {
 			English = "The Tracker is able to use a wallhack for 10 seconds once every 75 seconds. They have no passive ability.",
+			French = "Le Traqueur est capable d'utiliser un wallhack pendant 10 secondes une fois toutes les 75 secondes. Il n'a aucune capacité passive",	
 			Русский = "Трекер может использовать видение сквозь стены в течение 10 секунд каждые 75 секунд. У него нет пассивных способностей."
 		}
 	}
